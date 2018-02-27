70f75dcfd827f678550ac3bba0626ee0e2b4e606
kattisA <kattis.a@gmail.com>
2018-02-27 13:44:48 +0100

    Add the files
    Uploaded the files of the project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//https://support.microsoft.com/en-us/help/308359/how-to-write-a-simple-web-service-by-using-visual-c-net
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			localhost.Service myMathService = new localhost.Service();
+			Console.Write("2 + 4 = {0}", myMathService.Add(2, 4));
+		}
+	}
+}
